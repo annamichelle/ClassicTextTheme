66559cc719088215f693b7438e9806c6a9e37042
Anna Michelle <anna_michelle@msn.com>
2014-10-29 14:40:27 -0500

    Pulled in EmiglioRemix files
    From the ClassicTextExhibit branch here:
    https://github.com/annamichelle/EmiglioRemix

---
+++ custom.php
@@ -0,0 +1,95 @@
+<?php 
+
+function emiglio_exhibit_builder_page_nav($exhibitPage = null)
+{
+    if (!$exhibitPage) {
+        if (!($exhibitPage = get_current_record('exhibit_page', false))) {
+            return;
+        }
+    }
+
+    $exhibit = $exhibitPage->getExhibit();
+    $html = '<ul class="exhibit-page-nav navigation" id="secondary-nav">' . "\n";
+    $pages = $exhibit->getTopPages();
+    $htmlChild = '';
+    foreach ($pages as $page) {
+        $current = (exhibit_builder_is_current_page($page)) ? 'class="current"' : '';
+        $html .= "<li $current>" . exhibit_builder_link_to_exhibit($exhibit, $page->title, array(), $page);
+        if ($current) {
+            if ($page->countChildPages() > 0) {
+                $childPages = $page->getChildPages();
+                $html .= '<ul class="child-pages">';
+                    foreach ($childPages as $childPage) {
+                        $html .= "<li>" . exhibit_builder_link_to_exhibit($exhibit, $childPage->title, array(), $childPage) . '</li>';
+                    }
+                $html .= '</ul>';
+            }
+        }
+        else {
+            if ($page->countChildPages() > 0) {
+                $childPages = $page->getChildPages();
+                $htmlChild = '<ul class="child-pages">';
+                $currentChild = '';
+                    foreach ($childPages as $childPage) {
+                        $current = (exhibit_builder_is_current_page($childPage)) ? 'class="current"' : '';
+                        $currentChild .= $current;
+                        $htmlChild .= "<li $current>" . exhibit_builder_link_to_exhibit($exhibit, $childPage->title, array(), $childPage) . '</li>';
+                    }
+                $htmlChild .= '</ul>';
+                if (!$currentChild) {
+                    $htmlChild = '';
+                }
+            }
+        }
+        $html .= $htmlChild . '</li>';
+        $htmlChild = '';
+    }
+    $html .= '</ul>' . "\n";
+    $html = apply_filters('exhibit_builder_page_nav', $html);
+    return $html;
+}
+
+function emiglio_exhibit_builder_page_summary($exhibitPage = null)
+{
+    if(!$exhibitPage) {
+        $exhibitPage = get_current_record('exhibit_page');
+    }
+
+    $html = '<li>'
+            . '<a href="' . exhibit_builder_exhibit_uri(get_current_record('exhibit'), $exhibitPage) . '">'
+            . metadata($exhibitPage, 'title') . '</a>'
+            . '</li>';
+    return $html;
+}
+
+function return_to_exhibit(){
+    $back = htmlspecialchars($_SERVER['HTTP_REFERER']);
+    $html = '<a href="' . $back . '">Back to the Exhibit</a>';
+    return $html;
+}
+
+function emiglio_exhibit_builder_summary_accordion($exhibitPage = null)
+{
+    if (!$exhibitPage) {
+        $exhibitPage = get_current_record('exhibit_page');
+    }
+
+    $html = '<h3>' . metadata($exhibitPage, 'title') .'</h3>';
+
+    $children = $exhibitPage->getChildPages();
+    if ($children) {
+        $html .= '<div><a href="' . exhibit_builder_exhibit_uri(get_current_record('exhibit'), $exhibitPage) 
+                . '">' . metadata($exhibitPage, 'title') .'</a><ul>';
+        foreach ($children as $child) {
+            $html .= exhibit_builder_page_summary($child);
+            release_object($child);
+        }
+        $html .= '</ul></div>';
+    }
+    else {
+        $html .= '<div><a href="' . exhibit_builder_exhibit_uri(get_current_record('exhibit'), $exhibitPage) 
+                . '">' . metadata($exhibitPage, 'title') .'</a></div>';
+    }
+    return $html;
+}
+?>
---
+++ exhibit-builder/exhibits/summary.php
@@ -0,0 +1,53 @@
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+<script>
+    $(function() {
+        $( "#accordion" ).accordion({
+            collapsible: true, active: false, heightStyle: "content"
+        });
+    });
+</script>
+
+<?php echo head(array('title' => metadata('exhibit', 'title'), 'bodyclass'=>'exhibits summary')); ?>
+
+<div id="primary">
+<h1><?php echo metadata('exhibit', 'title'); ?></h1>
+
+<?php if ($exhibitDescription = metadata('exhibit', 'description', array('no_escape' => true))): ?>
+<div class="exhibit-description">
+    <?php echo $exhibitDescription; ?>
+</div>
+<?php endif; ?>
+
+<?php if (($exhibitCredits = metadata('exhibit', 'credits'))): ?>
+<div class="exhibit-credits">
+    <h3><?php echo __('Credits'); ?></h3>
+    <p><?php echo $exhibitCredits; ?></p>
+</div>
+<?php endif; ?>
+
+<?php set_exhibit_pages_for_loop_by_exhibit(); ?>
+<?php if (has_loop_records('exhibit_page')): ?>
+<div class="exhibit-contents">
+	<h3><?php echo __('Contents'); ?></h3>
+    <div id="accordion">
+        <?php foreach (loop('exhibit_page') as $exhibitPage): ?>
+        <?php echo emiglio_exhibit_builder_summary_accordion($exhibitPage); ?>
+        <?php endforeach; ?>
+    </div>
+</div>
+<?php endif; ?>
+</div>
+
+<?php if (has_loop_records('exhibit_page')): ?>
+<nav id="exhibit-pages">
+    <ul>
+        <?php foreach (loop('exhibit_page') as $exhibitPage): ?>
+        <?php echo emiglio_exhibit_builder_page_summary($exhibitPage); ?>
+        <?php endforeach; ?>
+    </ul>
+</nav>
+<?php endif; ?>
+
+<?php echo foot(); ?>
